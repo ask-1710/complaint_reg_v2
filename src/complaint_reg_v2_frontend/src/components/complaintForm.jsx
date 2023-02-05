6ca0447c37bb7a7f538227bb9afba8adcb901368
Aarthi Suresh Kumar <aarthisureshkumar17@gmail.com>
2023-02-05 16:38:10 +0530

    Add complaint form frontend

---
+++ src/complaint_reg_v2_frontend/src/components/complaintForm.jsx
@@ -0,0 +1,104 @@
+import DatePicker from "react-date-picker";
+import React, { useState } from "react";
+import { Card } from "../../../../node_modules/react-bootstrap/esm/index";
+
+const ComplaintForm = ({createActor, actor}) => {
+  const [newComplaint, setNewComplaint] = useState({
+    title: "",
+    summary: "",
+    location: "",
+    date: new Date(),
+  });
+
+
+  async function submitComplaint() {
+    console.log(actor);
+    console.log(newComplaint);
+    const isCreated = await actor.addComplaint(newComplaint.title, newComplaint.summary, newComplaint.location, newComplaint.date.toString());
+    if(isCreated) {
+        console.log("Complaint Created");
+    } else {
+        console.log("Error while creating complaint");
+    }
+    setNewComplaint("", "", "", newComplaint.date);
+
+  }
+
+  return (
+    <div className="container">
+      <div className="mx-auto">
+        <Card className="bg-light w-75 mx-auto">
+            <div className="row my-2 mx-5">
+              <h5>Fill in the details</h5>
+            </div>
+          <Card.Body>
+            <div className="row mt-2 justify-content-center">
+              <input
+                type="text"
+                className="form-control col-6"
+                placeholder="Enter title"
+                value={newComplaint.title}
+                onChange={(e) => {
+                  setNewComplaint({
+                    ...newComplaint,
+                    ["title"]: e.target.value,
+                  });
+                }}
+              ></input>
+            </div>
+
+            <div className="row mt-2 justify-content-center">
+              <textarea
+                className="col-6 wrap-content form-control"
+                placeholder="Enter summary"
+                wrap="soft"
+                value={newComplaint.summary}
+                onChange={(e) => {
+                  setNewComplaint({
+                    ...newComplaint,
+                    ["summary"]: e.target.value,
+                  });
+                }}
+              ></textarea>
+            </div>
+            <div className="row mt-2 justify-content-space-between">
+              <div className="col">
+                <DatePicker
+                  className="form-control col-3"
+                  placeholder="Date"
+                  value={newComplaint.date}
+                  onChange={(e) => {
+                    setNewComplaint({
+                      ...newComplaint,
+                      ["date"]: e,
+                    });
+                  }}
+                />
+              </div>
+
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control col-3"
+                  placeholder="Enter location"
+                  value={newComplaint.location}
+                  onChange={(e) => {
+                    setNewComplaint({
+                      ...newComplaint,
+                      ["location"]: e.target.value,
+                    });
+                  }}
+                ></input>
+              </div>
+            </div>
+            <div className="row mt-4">
+              <button className="button-27" onClick={submitComplaint}>Add complaint</button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default ComplaintForm;
--- src/complaint_reg_v2_frontend/src/pages/UserDashboard.jsx
+++ src/complaint_reg_v2_frontend/src/pages/UserDashboard.jsx
@@ -3,8 +3,17 @@ import Spinner from "react-bootstrap/Spinner";
 import { Badge } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import { idlFactory } from "../../../declarations/complaint_reg_v2_backend";
+import { Card } from "../../../../node_modules/react-bootstrap/esm/index";
+import CardHeader from "../../../../node_modules/react-bootstrap/esm/CardHeader";
+import ComplaintForm from "../components/complaintForm";
 
-const UserDashboard = ({ actor, setIsConnected , createActor , setIsNewUser, setIsSetupComplete }) => {
+const UserDashboard = ({
+  actor,
+  setIsConnected,
+  createActor,
+  setIsNewUser,
+  setIsSetupComplete,
+}) => {
   const nnsCanisterId = "rrkah-fqaaa-aaaaa-aaaaq-cai";
   const [user, setUser] = useState({
     name: "",
@@ -13,30 +22,32 @@ const UserDashboard = ({ actor, setIsConnected , createActor , setIsNewUser, set
   });
   const [complaints, setComplaints] = useState([]);
   const [isUserSet, setIsUserSet] = useState(false);
-  const possibleStages = {firregisteration: {step:1, badgeText:"FIR registeration"},investigation: {step:2, badgeText:"Investigation on-progress"}, finalreportfiling: {step:3, badgeText:"Filing final report"},solved: {step:4, badgeText:"Solved"}, unsolved: {step:5, badgeText:"Unsolved"}};
-  
-  const location = useLocation();
-  const { principalId } = location?.state  || {};
-  const { isConnected } = location?.state || {};
-  
+  const possibleStages = {
+    firregisteration: { step: 1, badgeText: "FIR registeration" },
+    investigation: { step: 2, badgeText: "Investigation on-progress" },
+    finalreportfiling: { step: 3, badgeText: "Filing final report" },
+    solved: { step: 4, badgeText: "Solved" },
+    unsolved: { step: 5, badgeText: "Unsolved" },
+  };
 
-  useEffect(()=>{
-    setIsSetupComplete(true);
-    setIsNewUser(false,"user");
-    setIsConnected(true);
-    if(actor=="") createActor();
-  },[])
+  const location = useLocation();
+  const { principalId } = location?.state || {};
+  const { isConnected } = location?.state || {};
 
   useEffect(() => {
-    if(!isUserSet && actor!="") getUserDetails();
+    setIsSetupComplete(true);
+    setIsNewUser(false, "user");
+    setIsConnected(true);
+    if (actor == "") createActor();
+  }, []);
+
+  useEffect(() => {
+    if (!isUserSet && actor != "") getUserDetails();
   }, [actor]);
 
-
   async function getUserDetails() {
-    
     // console.log("user is :"+principalId+":getUserDetails");
     const user = await actor.getUserDetails();
-
     const userComplaints = await actor.getUserComplaints();
 
     // const user = {
@@ -52,63 +63,128 @@ const UserDashboard = ({ actor, setIsConnected , createActor , setIsNewUser, set
     //   [3, { title: "abc", summary: "sum", date: "12/3/22", location: "delhi" ,  status:{unsolved : null} }],
     //   [4, { title: "abc", summary: "sum", date: "12/3/22", location: "delhi" ,  status:{finalreportfiling : null} }],
     // ];
+
     setUser(user[1]);
     setComplaints(userComplaints);
     setIsUserSet(true);
   }
 
+  const handleClickScrollToComplaintForm = () => {
+    const anchor = document.querySelector("#anchor");
+    anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+
+  const handleClickScrollToTop = () => {
+    const anchor = document.querySelector("#top");
+    anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+
   return (
-    <div className="body-container">
+    <div className="container">
       <div className="container">
-        <div className="jumbotron">
+        <div className="jumbotron d-flex justify-content-around" id="top">
           <div className="pt-4 display-2">My Dashboard</div>
+          <div className="pt-5 mr-2">
+            {isUserSet && (
+              <button
+                className="btn active"
+                type="button"
+                onClick={handleClickScrollToComplaintForm}
+              >
+                New complaint
+              </button>
+            )}
+          </div>
         </div>
       </div>
       <div className="container">
         {isUserSet ? (
-          complaints.length > 0 && (
-            <div className="jumbotron">
-              <div className="pt-4 display-6">Track your complaints</div>
-              <hr className="my-4"></hr>
-              <div className="container">
-                <div className="list-group">
-                  {complaints.map((complaint) => {
-                    return (
-                      <button
-                        key={complaint[0]}
-                        href="#"
-                        className="list-group-item my-2 list-group-item-action align-items-start"
-                        data-toggle="list"
-                      >
-                        <ul>
-                          <li>{complaint[1].title}</li>
-                          <li>{complaint[1].summary}</li>
-                          <li>{complaint[1].date}</li>
-                          <li>{complaint[1].location}</li>
-                          <li>{
-                                possibleStages[Object.keys(complaint[1].status)[0]].step < 2 ? (        
-                                    <Badge bg="warning" text="dark">{possibleStages[Object.keys(complaint[1].status)[0]].badgeText}</Badge>
-                                ):(                                
-                                    possibleStages[Object.keys(complaint[1].status)[0]].step < 4 ? (        
-                                    <Badge bg="info" text="dark">{possibleStages[Object.keys(complaint[1].status)[0]].badgeText}</Badge>
-                                    ):(
-                                        possibleStages[Object.keys(complaint[1].status)[0]].badgeText=="Solved" ? (   
-                                            <Badge bg="success" text="dark">{possibleStages[Object.keys(complaint[1].status)[0]].badgeText}</Badge>
-                                        ):(
-                                            <Badge bg="danger" text="dark">{possibleStages[Object.keys(complaint[1].status)[0]].badgeText}</Badge>
-                                        )
-                                    )
-                                )
-                            }
-                            </li>                            
-                        </ul>
-                      </button>
-                    );
-                  })}
+          <div>
+            {complaints.length > 0 && (
+              <div className="jumbotron">
+                <div className="pt-4 display-6">Track your complaints</div>
+                <hr className="my-4"></hr>
+                <div className="new-section-container container">
+                  <div className="list-group">
+                    {complaints.map((complaint) => {
+                      return (
+                        <button
+                          key={complaint[0]}
+                          href="#"
+                          className="list-group-item my-2 list-group-item-action align-items-start"
+                          data-toggle="list"
+                        >
+                          <ul>
+                            <li>{complaint[1].title}</li>
+                            <li>{complaint[1].summary}</li>
+                            <li>{complaint[1].date}</li>
+                            <li>{complaint[1].location}</li>
+                            <li>
+                              {possibleStages[
+                                Object.keys(complaint[1].status)[0]
+                              ].step < 2 ? (
+                                <Badge bg="warning" text="dark">
+                                  {
+                                    possibleStages[
+                                      Object.keys(complaint[1].status)[0]
+                                    ].badgeText
+                                  }
+                                </Badge>
+                              ) : possibleStages[
+                                  Object.keys(complaint[1].status)[0]
+                                ].step < 4 ? (
+                                <Badge bg="info" text="dark">
+                                  {
+                                    possibleStages[
+                                      Object.keys(complaint[1].status)[0]
+                                    ].badgeText
+                                  }
+                                </Badge>
+                              ) : possibleStages[
+                                  Object.keys(complaint[1].status)[0]
+                                ].badgeText == "Solved" ? (
+                                <Badge bg="success" text="dark">
+                                  {
+                                    possibleStages[
+                                      Object.keys(complaint[1].status)[0]
+                                    ].badgeText
+                                  }
+                                </Badge>
+                              ) : (
+                                <Badge bg="danger" text="dark">
+                                  {
+                                    possibleStages[
+                                      Object.keys(complaint[1].status)[0]
+                                    ].badgeText
+                                  }
+                                </Badge>
+                              )}
+                            </li>
+                          </ul>
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            )}
+            <div id="anchor" className="new-section-container">
+              <div className="new-section-center">
+                <div className="mb-5">
+                  <ComplaintForm actor={actor} createActor={createActor} />
+                </div>
+                <div className="mt-4 d-flex flex-row-reverse">
+                  <button
+                    className="btn active"
+                    type="button"
+                    onClick={handleClickScrollToTop}
+                  >
+                    Back to top
+                  </button>
                 </div>
               </div>
             </div>
-          )
+          </div>
         ) : (
           <div className="center">
             <Spinner animation="border">
