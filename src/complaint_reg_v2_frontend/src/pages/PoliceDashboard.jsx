053964935ab091786456ca60a675539feddc08c7
Aarthi Suresh Kumar <aarthisureshkumar17@gmail.com>
2023-02-10 23:09:52 +0530

    Fix view complaints status in police dashboard

--- src/complaint_reg_v2_frontend/src/pages/PoliceDashboard.jsx
+++ src/complaint_reg_v2_frontend/src/pages/PoliceDashboard.jsx
@@ -3,28 +3,42 @@ import { useLocation } from "react-router-dom";
 import { complaint_reg_v2_backend } from "../../../declarations/complaint_reg_v2_backend";
 import { idlFactory } from "../../../declarations/complaint_reg_v2_backend";
 import { useEffect } from "react";
+import { Badge } from "react-bootstrap";
 
-const PoliceDashboard = ({actor , setIsConnected, createActor, setIsNewUser, setIsSetupComplete}) => {
+const PoliceDashboard = ({
+  actor,
+  setIsConnected,
+  createActor,
+  setIsNewUser,
+  setIsSetupComplete,
+}) => {
   const nnsCanisterId = "rrkah-fqaaa-aaaaa-aaaaq-cai";
   const [complaints, setComplaints] = useState([]);
   const [complaintId, setComplaintId] = useState(0);
   const location = useLocation();
+  const possibleStages = {
+    firregisteration: { step: 1, badgeText: "FIR registeration" },
+    investigation: { step: 2, badgeText: "Investigation on-progress" },
+    finalreportfiling: { step: 3, badgeText: "Filing final report" },
+    solved: { step: 4, badgeText: "Solved" },
+    unsolved: { step: 5, badgeText: "Unsolved" },
+  };
 
   const principalId = location?.state?.principalId;
   const isConnected = location?.state?.isConnected;
 
   useEffect(() => {
     setIsSetupComplete(true);
-    setIsNewUser(false,"police");
+    setIsNewUser(false, "police");
     setIsConnected(true);
-    
-    if (actor == "") createActor();
-  },[]);
 
-  useEffect(()=>{
-    if(actor!="") getUnassignedComplaints();
-  },[actor])
-  
+    if (actor == "") createActor();
+  }, []);
+
+  useEffect(() => {
+    if (actor != "") getUnassignedComplaints();
+  }, [actor]);
+
   async function getUnassignedComplaints() {
     var complaints = await complaint_reg_v2_backend.getUnassignedComplaints();
     console.log(complaints);
@@ -61,13 +75,58 @@ const PoliceDashboard = ({actor , setIsConnected, createActor, setIsNewUser, set
                   className="list-group-item my-2 list-group-item-action align-items-start"
                   data-toggle="list"
                 >
-                  <ol>
-                    <li>{complaint[1].title}</li>
-                    <li>{complaint[1].summary}</li>
-                    <li>{complaint[1].date}</li>
-                    <li>{complaint[1].location}</li>
-                    <li>{complaint[1].status}</li>
-                  </ol>
+                  <ul className="complaint-list">
+                    <li>
+                      <b>Short description</b>
+                      {" : " + complaint[1].title}
+                    </li>
+                    <li>
+                      <b>Summary of incident</b>
+                      {" : " + complaint[1].summary}
+                    </li>
+                    <li>
+                      <b>Date of occurence</b>
+                      {" : " + complaint[1].date}
+                    </li>
+                    <li>
+                      <b>Location of occurence</b>
+                      {" : " + complaint[1].location}
+                    </li>{" "}
+                    <li>
+                      {possibleStages[Object.keys(complaint[1].status)[0]]
+                        .step < 2 ? (
+                        <Badge bg="warning" text="dark">
+                          {
+                            possibleStages[Object.keys(complaint[1].status)[0]]
+                              .badgeText
+                          }
+                        </Badge>
+                      ) : possibleStages[Object.keys(complaint[1].status)[0]]
+                          .step < 4 ? (
+                        <Badge bg="info" text="dark">
+                          {
+                            possibleStages[Object.keys(complaint[1].status)[0]]
+                              .badgeText
+                          }
+                        </Badge>
+                      ) : possibleStages[Object.keys(complaint[1].status)[0]]
+                          .badgeText == "Solved" ? (
+                        <Badge bg="success" text="dark">
+                          {
+                            possibleStages[Object.keys(complaint[1].status)[0]]
+                              .badgeText
+                          }
+                        </Badge>
+                      ) : (
+                        <Badge bg="danger" text="dark">
+                          {
+                            possibleStages[Object.keys(complaint[1].status)[0]]
+                              .badgeText
+                          }
+                        </Badge>
+                      )}
+                    </li>
+                  </ul>
                 </button>
               );
             })}
